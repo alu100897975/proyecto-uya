900c23027c334a159d43822ccf4a8654ab590826
Alex Jimmy <alu0100728170@ull.edu.es>
2017-05-05 17:16:32 +0100

    Pagina principal, registro y login implementados

---
+++ app.js
@@ -0,0 +1,31 @@
+'use strict'
+var express = require('express'),
+    bodyParser = require('body-parser'),
+
+    app = express(),
+    port = process.env.PORT || 8080;
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'pug');
+
+app.use(bodyParser.urlencoded({extended: true})); //form params in req.body
+
+app.use(express.static('public'));
+
+app.get('/', (req,res)=>{
+    res.render('index');
+});
+app.get('/signup', (req,res)=>{
+    res.render('signup');
+});
+app.get('/signin', (req,res)=>{
+    res.render('signin');
+})
+
+app.listen(port, (err)=>{
+    if(err){
+        console.log('error:',err);
+        return;
+    }
+    console.log('Servidor corriendo en el puerto '+port);
+})
