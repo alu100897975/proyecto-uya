23709e69980523313e2b66f1440986a76e4a0453
Alex Jimmy <alu0100728170@ull.edu.es>
2017-05-07 17:30:09 +0100

    Utilizacion de sass para desarrollo

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict'
 var express = require('express'),
     bodyParser = require('body-parser'),
+    sass = require('node-sass-middleware'),
 
     app = express(),
     port = process.env.PORT || 8080;
@@ -8,6 +9,11 @@ var express = require('express'),
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
 
+app.use(sass({
+    src: __dirname + '/development',
+    dest: __dirname + '/public',
+    debug: true
+}));
 app.use(bodyParser.urlencoded({extended: true})); //form params in req.body
 
 app.use(express.static('public'));
