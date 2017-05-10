0532300b40905d86daaa50a4df8e9131d34eb7af
Alex Jimmy <alu0100728170@ull.edu.es>
2017-05-10 15:32:44 +0100

    Credenciales usuario de prueba Facebook

--- auth/auth.js
+++ auth/auth.js
@@ -4,7 +4,7 @@ module.exports = {
         clientSecret: 'zzifHovLQKwkBU47Kbd_kzzM',
         callbackURL: 'http://localhost:8080/join/with-google/return'
     },
-    facebookAuth: {
+    facebookAuth: { // usuario prueba => email: usyelurbds_1494426217@tfbnw.net, password: UsuarioPrueba
         clientID: '1903921206544991',
         clientSecret: '29d0f049d271069077b6a7711fea0f3d',
         callbackURL: 'http://localhost:8080/join/with-facebook/return'
